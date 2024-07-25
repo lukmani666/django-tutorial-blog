010d1441148e3c81c9751ff3e7eb3f2ffc4b0e55
ola-dev <olamidelukman3@gmail.com>
2024-07-25 12:57:19 +0100

    fixed

--- blogconfiguration/settings.py
+++ blogconfiguration/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-tutorial-blog-1.onrender.com']
 
 
 # Application definition
