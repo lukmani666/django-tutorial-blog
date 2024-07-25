4930509f232c798f9798a29c3660d8a71a4937a1
ola-dev <olamidelukman3@gmail.com>
2024-07-25 14:49:25 +0100

    add-build-sh-toyt

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 pip install -r requirements.txt
+python manage.py showmigrations
 python manage.py migrate
-python manage.py collectstatic
+python manage.py showmigrations
+python manage.py collectstatic --noinput
 
