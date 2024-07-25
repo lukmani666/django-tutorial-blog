e6ffd5da559410df673bbd1bd57eff326ada99ed
ola-dev <olamidelukman3@gmail.com>
2024-07-25 13:50:19 +0100

    add-build-sh-to

--- blogconfiguration/settings.py
+++ blogconfiguration/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 from decouple import config
 import dj_database_url
@@ -113,6 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
